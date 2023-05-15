a17c0a86e8d65dd4dff406ff9219aaa8ebd84298
balazssagi <balazssagi@outlook.com>
2023-05-15 21:27:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write(JSON.stringify(process.env));
   res.end();
 }).listen(80);
